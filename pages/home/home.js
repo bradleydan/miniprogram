c80b0e7ffcbe05e8dc2c03a9a5fc9029a58bda53
bradleydan <894145261@qq.com>
2020-05-31 19:27:44 +0800

    初体验

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,6 +1,22 @@
 Page({
  data: {
    name:'bradley',
-   age:18
+   age:18,
+   students:[
+     {id:1,name:'a',age:20},
+     {id:2,name:'aa',age:20},
+     {id:3,name:'aaa',age:40}
+    ],
+    counter:0
+ },
+ add() {
+   this.setData({
+       counter: this.data.counter+1
+   })
+ },
+ sub(){
+   this.setData({
+     counter:this.data.counter-1
+   })
  }
 })
